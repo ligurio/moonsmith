c46042ab6e597c529071b8095d5ad528d9a362d7
Sergey Bronnikov <estetus@gmail.com>
2026-02-13 17:20:19 +0300

    src: fix bitwise operators

--- lua/lib.lua
+++ lua/lib.lua
@@ -1,17 +1,40 @@
 --------------------------------------------------------
 -- Utility functions.                                 --
 --------------------------------------------------------
-ms = {}
+
+local function bitwise_op(op_name)
+    return function(...)
+        local n = select("#", ...)
+        local chunk
+        -- Bitwise exclusive OR and bitwise NOT have the same
+        -- operator.
+        if (op_name == "&" or op_name == "|") then
+            assert(n > 1)
+        end
+        if n == 1 then
+            local x = ...
+            chunk = ("return %s %d"):format(op_name, x)
+        else
+            local op_name_ws = (" %s "):format(op_name)
+            chunk = "return " .. table.concat({...}, op_name_ws)
+        end
+        return assert(load(chunk))()
+    end
+end
+
+local band = bitwise_op("&")
+
+local ms = {}
 
 -- Generates a random integer for an arbitrary table.
 -- @param t table
 function ms.table_to_int(t)
-  acc = 0
+  local acc = 0
   for i, v in ipairs(t) do
     if type(v) == "number" then
       acc = acc + v
     else
-      acc = acc & i
+      acc = band(acc, i)
     end
   end
   return acc + #t
